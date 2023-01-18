9cd51b09b76997bffe689ca343bb923063026e27
Elena Poganova <a.n.rus@mail.ru>
2023-01-18 10:14:19 +0400

    Добавлена папка Work3

---
+++ Work3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 6 || n == 7)
+    Console.WriteLine("Выходной");
+else
+    Console.WriteLine("Нет");
