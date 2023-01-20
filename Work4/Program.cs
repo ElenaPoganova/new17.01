0aa4f4c1745bd0f225878b23841b45a42bcb39e9
Elena Poganova <a.n.rus@mail.ru>
2023-01-20 10:33:03 +0400

    Work4

---
+++ Work4/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine()), max = n, count = 1;
+while (n > 0 && n <= 100 && count <= n && n != 0)
+{ n = Convert.ToInt32(Console.ReadLine());
+    {
+    if (max < count) 
+        max = count;
+        count = count + 1;
+    }  
+    max = 1;
+    {
+        int max2 = count;   
+        if (max2 < count)
+            count = count + 1;
+        Console.Write($"{max2}");    
+    }
+   
+}*/
