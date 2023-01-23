b2032dc47f28df7b9dec9960d0b5d97e6acd5776
Elena Poganova <a.n.rus@mail.ru>
2023-01-23 16:07:40 +0400

    WorkN1

---
+++ WorkN1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n % 10;
+int n2 = n / 10000;
+int n3 = n / 10;
+int n4 = n / 1000;
+if (n1 == n2 && (n3 % 10) == (n4 % 10))
+   Console.WriteLine("Это палиндром");
+else
+    Console.WriteLine("Это не палиндром");
